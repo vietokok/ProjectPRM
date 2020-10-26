f7a65e9fd88a8ecc24627c5f3c0800d66bb46fa0
dark <tuan150419999@gmail.com>
2020-10-26 22:41:53 +0700

    12

--- app/src/main/java/com/example/firebaseis1313/MainActivity.java
+++ app/src/main/java/com/example/firebaseis1313/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Bo Tuan");
+        System.out.println("Ngo Ha");
         viewPager=findViewById(R.id.view_page);
         tabLayout=findViewById(R.id.tab_layout);
 
